9512038b21a86d4efe8d4f73d0c12ed6821558f7
Jules Dieu donné - G3-YG <mdjulesdeveloper23@gmail.com>
2026-05-31 19:02:26 +0000

    Real update of the app

--- S1/mvc/test/widget_test.dart
+++ S1/mvc/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:anti_mvc/main.dart';
+import 'package:mvc/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
