0dc133c79757869e6d831ccae35ac6aeb11f601d
Jules Dieu donné - G3-YG <mdjulesdeveloper23@gmail.com>
2026-05-31 21:31:38 +0000

    Removing the comments

--- S1/streaming/lib/controllers/emission_controller.dart
+++ S1/streaming/lib/controllers/emission_controller.dart
@@ -5,44 +5,6 @@ import '../models/diffusion.dart';
 
 class EmissionController {
   final List<Emission> _emissions = mockEmissions;
-  // final List<Emission> _emissions = [
-  //   const Emission(
-  //     id: 1,
-  //     nom: 'Morning News',
-  //     chaine: 'Radio 4',
-  //     image: 'assets/images/news.png',
-  //   ),
-  //   const Emission(
-  //     id: 2,
-  //     nom: 'Music Time',
-  //     chaine: 'FM Music',
-  //     image: 'assets/images/music.avif',
-  //   ),
-  //   const Emission(
-  //     id: 3,
-  //     nom: 'Sport Live',
-  //     chaine: 'Sport Radio',
-  //     image: 'assets/images/sport.avif',
-  //   ),
-  //   const Emission(
-  //     id: 4,
-  //     nom: 'Tech Talk',
-  //     chaine: 'Digital FM',
-  //     image: 'assets/images/tech.avif',
-  //   ),
-  //   const Emission(
-  //     id: 5,
-  //     nom: 'Culture Plus',
-  //     chaine: 'Culture TV',
-  //     image: 'assets/images/culture.jpg',
-  //   ),
-  //   const Emission(
-  //     id: 6,
-  //     nom: 'Business Show',
-  //     chaine: 'Finance Radio',
-  //     image: 'assets/images/business.webp',
-  //   ),
-  // ];
 
   List<Emission> getEmissions() {
     return _emissions;
